6c67af9d95f3cbc59f053b4e34f669f1de4f9026
Thomas Dutrion <thomas@engineor.com>
2016-06-07 23:31:04 +0100

    clean the project

--- RoboFile.php
+++ RoboFile.php
@@ -6,44 +6,98 @@
  */
 class RoboFile extends \Robo\Tasks
 {
-    public function build()
+    /**
+     * Build the project (dev by default)
+     *
+     * @param array $opts
+     * @option $dist Set whether buildDist should be executed
+     */
+    public function build($opts = ['dist|d' => false])
     {
+        $this->buildDev();
+        if (isset($opts['dist']) && $opts['dist']) {
+            $this->buildDist();
+        }
+    }
+
+    public function buildDev()
+    {
+        // remove existing dev and dist folder
         $this->clear();
-        $this->taskFileSystemStack()
-            ->mkdir('dist')
-            ->run();
+
+        // recreate the dev folder
+        $this->_mkdir('dev');
+
+        // copy required files
+        $this->_copyDir('pages', 'dev');
+        $this->_copyDir('images', 'dev');
+        $this->_copyDir('favicon', 'dev');
+
+        // compile scss
+        $this->compileScss('assets/sass/ie8.scss', 'dev/ie8.css');
+        $this->compileScss('assets/sass/ie9.scss', 'dev/ie9.css');
+        $this->compileScss('assets/sass/leaflet.scss', 'dev/leaflet.css');
+        $this->compileScss('assets/sass/main.scss', 'dev/main.css');
+
+        // fetch javascript
         $this->taskFlattenDir([
-            'assets/*' => 'dist',
-            'pages/*.html' => 'dist',
-            'favicon/*' => 'dist',
+            'assets/js' => 'dev',
         ])->run();
+    }
+
+    public function buildDist()
+    {
+        // remove existing dist folder
+        $this->_deleteDir('dist');
+
+        // recreate the dist folder
+        $this->_mkdir('dist');
+
+        // copy pages
+        $this->_copyDir('dev', 'dist');
+
+        foreach (glob('dist/*.css') as $cssFile) {
+            $this->taskMinify($cssFile)
+                ->to($cssFile)
+                ->run();
+        }
     }
 
     public function clear()
     {
-        $this->taskDeleteDir('dist')->run();
+        $this->taskDeleteDir([
+            'dev',
+            'dist',
+        ])->run();
     }
 
     public function watch()
     {
+        // build dev environment
+        $this->build();
+
         $this->taskWatch()
-            ->monitor('assets/sass/main.scss', function() {
-                $this->compileScss(
-                    'assets/sass/main.scss',
-                    'assets/css/main.css'
-                );
+            ->monitor('assets/sass', function() {
+                foreach (glob('assets/sass/*.scss') as $file) {
+                    $this->compileScss(
+                        $file,
+                        basename($file, '.scss') . '.css'
+                    );
+                }
             })
-            ->monitor('assets/sass/ie8.scss', function() {
-                $this->compileScss(
-                    'assets/sass/ie8.scss',
-                    'assets/css/ie8.css'
-                );
+            ->monitor('assets/js', function() {
+                $this->taskFlattenDir([
+                    'assets/js' => 'dev',
+                ])->run();
             })
-            ->monitor('assets/sass/ie9.scss', function() {
-                $this->compileScss(
-                    'assets/sass/ie9.scss',
-                    'assets/css/ie9.css'
-                );
+            ->monitor('pages', function() {
+                $this->_copyDir('pages', 'dev');
+            })
+            ->monitor('favicon', function() {
+                $this->_copyDir('favicon', 'dev');
+            })
+            ->monitor('images', function() {
+                $this->_copyDir('images', 'dev');
             })
         ->run();
     }
@@ -52,33 +106,14 @@ class RoboFile extends \Robo\Tasks
      * @param $src
      * @param $dest
      */
-    public function compileScss($src, $dest)
+    private function compileScss($src, $dest)
     {
-        $this->taskScss([
+        // waiting for https://github.com/leafo/scssphp/issues/409
+        /*$this->taskScss([
             $src => $dest
         ])
         ->setImportPaths([dirname($src)])
-        ->run();
-    }
-
-    public function minifyCss()
-    {
-        $this->taskMinify( 'assets/css/main.css' )
-            ->run();
-    }
-
-    public function minifyJs()
-    {
-
-    }
-
-    public function concatenateCss()
-    {
-
-    }
-
-    public function concatenateJs()
-    {
-
+        ->run();*/
+        $this->_exec("sass {$src} {$dest}");
     }
 }
